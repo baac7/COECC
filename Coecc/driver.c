3e7a2e8d11b690773f443ae8560e672a5acfe963
baac7 <acarbatuhan.42@gmail.com>
2021-03-31 17:36:45 +0300

    Update driver.c

--- Coecc/driver.c
+++ Coecc/driver.c
@@ -8,11 +8,15 @@ int main(){
 
 	setup();
 
-	tecPoint testPoint1;
+	tecPoint testPoint1, testPoint2;
 
 	init_normalCoord(&testPoint1, "162364632", "27347237423");
 
-	functionToBeNamed1(91, &testPoint1);
+	//testPoint2 = coecc_scalarMul(91, &testPoint1);
+	//gmp_printf("%Zd\n%Zd", testPoint2.x, testPoint2.y);
+
+	keyPairGeneration(&testPoint1);
 
 	return 0;
 }
+
